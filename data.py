897cfaa3ff8192db33d279e2bfa440ced1e4f3d5
Anna Mugume <mugumeshalom@gmail.com>
2025-10-30 10:42:50 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,73 @@
+question_data = [
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "The alternative rock band, They Might Be Giants, released their album 'Flood' in 1990. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Korean girl group, TWICE, consists of 9 members",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Eurobeat is primarily an Italian genre of music.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "The music group Daft Punk got their name from a negative review they received.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Daft Punk originated in France.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Stevie Wonder's;s real name is Stevland Hardaway Morris.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "A Saxophone is a brass instrument.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "The music video to The Buggles' 'Video Killed the Radio Star'; was the first music video to broadcast on MTV.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean", "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "The 2011 movie 'The Adventures of Tintin' was directed by Steven Spielberg.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]}
+]
